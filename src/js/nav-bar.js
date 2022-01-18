b0742ef59057f5825b85cc01974b9db994a10e26
Laura Findley <laurjanefindley@gmail.com>
2022-01-18 22:01:59 +0000

    Adapt script to work with list-based nav structure

--- src/js/nav-bar.js
+++ src/js/nav-bar.js
@@ -1,8 +1,15 @@
+//Modified from W3 School's Responsive Topnav Example: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_topnav
 function toggleNavMenu() {
     var menu = document.getElementById("nav-menu");
-    if (x.className === "nav-menu") {
-      x.className += " responsive";
+    if (menu.className === "nav-menu") {
+      menu.className += " responsive";
+
+      var navItems = menu.getElementsByClassName("nav-item");
+      for (i = 0; i < navItems.length; i++) navItems[i].className += " responsive";
     } else {
-      x.className = "nav-menu";
+      menu.className = "nav-menu";
+      
+      var navItems = menu.getElementsByClassName("nav-item responsive");
+      while (navItems.length > 0) navItems[0].className = "nav-item";
     }
-  }
+}
